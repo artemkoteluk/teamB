cee680ff76f1684aa349d3f8c43991721ea78e75
Venzard <assasin0084@gmail.com>
2021-11-24 14:13:31 +0200

    routs start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {MainComponent} from "./main/main.component";
+
+const routes: Routes=[
+  {
+    path:'main',
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule),
+  },
+  // {
+  //   path:'login',
+  //   loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+  // },
+  // {
+  //   path:'register',
+  //   loadChildren: () => import('./register/register.module').then(m => m.RegisterModule),
+  //
+  // },
+  {
+    path:'**',
+    redirectTo:'',
+    pathMatch:'full'
+
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
-  ],
+    RouterModule.forRoot(routes),
+    BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
