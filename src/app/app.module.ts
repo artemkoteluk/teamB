7461f7e0bb8ff078e7d38d995567da3c10f0323e
Olga Smachna <smachna.ov@gmail.com>
2021-11-25 23:48:35 +0200

    table finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
-import { NgModule } from '@angular/core';
+import {InjectionToken, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NotifierModule } from 'angular-notifier';
+import {RouterModule, Routes} from "@angular/router";
+import {MainComponent} from "./main/main.component";
+import {MatDialog, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import {Overlay} from "@angular/cdk/overlay";
+import {TableDialog} from "./table/table.component";
+
+const routes: Routes=[
+  {
+    path:'main',
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule),
+  },
+  // {
+  //   path:'login',
+  //   loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+  // },
+  // {
+  //   path:'register',
+  //   loadChildren: () => import('./register/register.module').then(m => m.RegisterModule),
+  //
+  // },
+  {
+    path:'',
+    redirectTo:'main',
+    pathMatch:'full'
+
+  },
+  {
+    path:'**',
+    redirectTo:'',
+    pathMatch:'full'
+
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TableComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    NotifierModule.withConfig({
+      // Custom options in here
+    }),
+    RouterModule.forRoot(routes),
+    ],
+  providers: [MatDialog, Overlay],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
